14a6ecef5dc703adddb7155de3ef9c2982614681
MrPelmenis <misterpelmenis@gmail.com>
2023-10-15 12:42:20 +0300

    progress on signup fixing

--- naggers/src/pages/login.js
+++ naggers/src/pages/login.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 
 import configData from "../config.json";
 
+import { ExtraFunctions } from "../extraFunctions";
+
 const Login = () => {
     const navigate = useNavigate();
 
@@ -11,7 +13,7 @@ const Login = () => {
 
         setTimeout(() => {
             window.location.href = "/";
-        }, 1000);
+        }, 5000);
 
     });
 
@@ -24,38 +26,42 @@ const Login = () => {
 
 
 async function callback() {
-    const url = new URL(window.location.href);
-    let code = url.searchParams.get('code');
-    console.log(code);
-
-
-    let response = await fetch('https://oauth2.googleapis.com/token', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: new URLSearchParams({
-            'code': code,
-            'client_id': configData.CLIENT_ID,
-            'client_secret': configData.CLIENT_SECRET,
-            'redirect_uri': configData.REDIRECT_URL,
-            'grant_type': 'authorization_code'
-        })
-    });
-
-    var result = await response.json();
-
-    console.log('response:');
-    console.log(result);
-
-
-    console.log("Token:");
-    // alert(JSON.stringify(result.id_token));
-    localStorage.setItem("JWT", JSON.stringify(result.id_token));
     let jtoken = (localStorage.getItem("JWT"));
-    console.log("from   storage jwt:");
-    console.log(jtoken);
-
+        console.log("from   storage jwt:");
+        console.log(jtoken);
+    if(!ExtraFunctions.isUserLoggedIn() && jtoken == "undefined"){
+        const url = new URL(window.location.href);
+        let code = url.searchParams.get('code');
+        console.log(code);
+    
+    
+        let response = await fetch('https://oauth2.googleapis.com/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: new URLSearchParams({
+                'code': code,
+                'client_id': configData.CLIENT_ID,
+                'client_secret': configData.CLIENT_SECRET,
+                'redirect_uri': configData.REDIRECT_URL,
+                'grant_type': 'authorization_code'
+            })
+        });
+    
+        var result = await response.json();
+    
+        console.log('response:');
+        console.log(result);
+    
+    
+        console.log("Token:");
+        alert(JSON.stringify(result.id_token));
+        localStorage.setItem("JWT", JSON.stringify(result.id_token));
+        jtoken = (localStorage.getItem("JWT"));
+        console.log("from   storage jwt:");
+        console.log(jtoken);
+    }
 }
 
 function parseJwt(token) {

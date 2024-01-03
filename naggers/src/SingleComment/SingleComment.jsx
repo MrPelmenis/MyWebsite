dfa31258e4c3fa02307795d3c17c6e0805ba7081
MrPelmenis <46497841+MrPelmenis@users.noreply.github.com>
2024-01-03 15:53:56 +0200

    podzinas commentaram sataisitas
    tagad lodzini, funckcionalitate

--- naggers/src/SingleComment/SingleComment.jsx
+++ naggers/src/SingleComment/SingleComment.jsx
@@ -16,6 +16,10 @@ import { AiFillLike } from "react-icons/ai";
 
 import { ExtraFunctions } from '../extraFunctions';
 
+import { MdEdit } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
+import { MdMoreHoriz } from "react-icons/md";
+
 export default function SingleComment(props) {
 
     const [isCommentLikedByUser, setIsCommentLikedByUser] = useState(props.isCommentLikedByCurrentUser);
@@ -37,6 +41,22 @@ export default function SingleComment(props) {
     }
 
 
+    const correctEditDeleteCommentButtons = () => {
+        return (<div className='editAndDelete'>
+                <div className='comment' onClick={()=>{alert("delete")}} >
+                    <MdEdit style={{ width:"25px", height:"25px" }}/>
+                </div>
+
+                <span>|</span>
+    
+                <div className='comment' onClick={()=>{}}>
+                    <MdDelete  style={{width:"25px", height:"25px"}}/>
+                </div>
+            </div>)
+    }
+
+
+
     return (
         <div className='SingleComment'>
             <div className='CommentAuthorDateInfo'>
@@ -50,13 +70,57 @@ export default function SingleComment(props) {
             <div className='commentText'><TextWithReadMoreButton text={props.text}></TextWithReadMoreButton></div>
 
             <div className='CommentLikesAndComments'>
-            <div className='CommentlikeIconAndCount' onClick={()=>{commentLikeCallback()}} >
-                    {isCommentLikedByUser != 1 ? (<AiOutlineLike style={{ width:"25px", height:"25px" }}/>) : (<AiFillLike style={{ width:"25px", height:"25px" }}/>)}
-                    <span style={{margin:"auto"}}>{currentLikeAmount}</span>
+                <div className='CommentlikeIconAndCount' onClick={()=>{commentLikeCallback()}} >
+                        {isCommentLikedByUser != 1 ? (<AiOutlineLike style={{ width:"25px", height:"25px", marginTop: "2px" }}/>) : (<AiFillLike style={{ width:"25px", height:"25px" }}/>)}
+                        <span style={{margin:"auto"}}>{currentLikeAmount}</span>
                 </div>
+                <HiddenEditDelteCommentButtons showMe={(props.authorName == currentUser)} title={props.title} body={props.body} commentID={props.id} />
             </div>
         </div>
     )
 }
 
+
+
+
+function HiddenEditDelteCommentButtons({showMe, title, body, PostId}){
+    const [show, setShow] = useState(showMe);
+    const dispatch = useDispatch();
+
+    const showEditPostScreen = ()=>{
+        alert("editcomment");
+    }
+
+    const showDeletePostScreen =()=>{
+        alert("deleteComment");
+    }
+
+    const correctEditDeleteButtons = () => {
+        return (<div className='hiddenEditDeleteButtonContainer'>
+                <div className='myIcon' onClick={showEditPostScreen} >
+                    <MdEdit style={{ width:"25px", height:"25px" }}/>
+                </div>
+
+                <span style={{padding:2, paddingTop:0}}>|</span>
+    
+                <div className='myIcon' onClick={()=>{ showDeletePostScreen()  }}>
+                    <MdDelete  style={{width:"25px", height:"25px"}}/>
+                </div>
+            </div>)
+    }
+
+    if(!show){
+        return(
+            <div onClick={()=>setShow(!show)} className='hiddenEditDeleteComment'><MdMoreHoriz  style={{width:25,height: 25, margin:"auto"}}/></div>
+        )
+    }else{
+        return(
+            <div className='editDeleteHideCommentcontainer'>
+                {correctEditDeleteButtons()}
+                <div onClick={()=>setShow(!show)} className='hiddenEditDeleteComment'><MdMoreHoriz  style={{width:25,height: 25, margin:"auto", backgroundColor: "rgb(185, 183, 183)", borderRadius:50}}/></div>
+            </div>
+        )
+    }
+
+}
 
